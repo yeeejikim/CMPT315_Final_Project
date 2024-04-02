539049959196f231a3b8112b1f016c0aff8cdb67
Anthony1Nguyen1 <Anthony1Nguyen1@hotmail.com>
2024-04-01 22:12:37 -0600

    Change file name, Fix bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Restaurants from './pages/Restaurants.js';
+import Restaurants from './pages/Restaurants.jsx';
 import Manager from './pages/Manager.jsx';
 import Menu from './pages/Menus.jsx';
 import Header from './header-component/Header.jsx';
