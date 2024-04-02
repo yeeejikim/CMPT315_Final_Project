cb344a952d77dbaa235e06b91c30aabc63f19ad3
Anthony1Nguyen1 <Anthony1Nguyen1@hotmail.com>
2024-04-01 22:26:35 -0600

    Fix card bug

--- frontend/src/components/card/restcard.component.jsx
+++ frontend/src/components/card/restcard.component.jsx
@@ -6,7 +6,7 @@ export const Card = ({ restaurant }) => {
     
     return (
         <Link to={`/menu/${rest_id}`} className='card-link'>
-            <div className='card-container'>
+            <div className='rest-card-container'>
                 <img alt='restaurant' src={rest_image} width={100} height={100}></img>
                 <h2>{rest_name}</h2>
             </div>
